28d7ae24c554e58f984322efade653c1fe4434af
joaocorreia1 <jpedrocbarros@gmail.com>
2023-03-30 22:06:51 -0300

    second step

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/images/logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>Seminários, Simpósios e Cursos Técnicos</p>
+        <a className="App-link" href="/" rel="noopener noreferrer">
+          Próximos Eventos
         </a>
       </header>
     </div>
