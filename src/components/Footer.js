31b3afc70d3078152f295d8f20cbbefd7463f5da
joaocorreia1 <jpedrocbarros@gmail.com>
2023-04-02 13:44:48 -0300

    about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import FacebookIcon from "../assets/images/facebook.svg";
 import InstagramIcon from "../assets/images/instagram.svg";
 import LinkedinIcon from "../assets/images/linkedin.svg";
+import EmailIcon from "../assets/images/email.svg";
+import WhatsappIcon from "../assets/images/whatsapp.svg";
 
 export class Footer extends Component {
   render() {
@@ -10,17 +12,19 @@ export class Footer extends Component {
       <footer className="bg-light text-center fixed-bottom main-footer mx-auto">
         <div className="container p-2">
           <div className="row">
-            <Link
-              to={"https://www.facebook.com/trabalhoevida"}
-              className="col-4"
+            <div className="col-1"></div>
+            <a
+              href="mailto:institutotrabalhoevida@gmail.com"
               target="_blank"
+              title="Send an email"
+              className="col-2"
               rel="noopener noreferrer"
             >
-              <img src={FacebookIcon} alt="Facebook" style={{ maxWidth: 24 }} />
-            </Link>
+              <img src={EmailIcon} alt="E-mail" style={{ maxWidth: 24 }} />
+            </a>
             <Link
               to={"https://www.instagram.com/institutotrabalhoevida/"}
-              className="col-4"
+              className="col-2"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -32,19 +36,38 @@ export class Footer extends Component {
             </Link>
             <Link
               to={"https://www.linkedin.com/company/instituto-trabalho-e-vida/"}
-              className="col-4"
+              className="col-2"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={LinkedinIcon} alt="Linkedin" style={{ maxWidth: 24 }} />
             </Link>
+            <Link
+              to={"https://www.facebook.com/trabalhoevida"}
+              className="col-2"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={FacebookIcon} alt="Facebook" style={{ maxWidth: 24 }} />
+            </Link>
+            <a
+              href="whatsapp://send?phone=5511941215530"
+              target="_blank"
+              title="Send an email"
+              className="col-2"
+              rel="noopener noreferrer"
+            >
+              <img src={WhatsappIcon} alt="Whatsapp" style={{ maxWidth: 24 }} />
+            </a>
+            <div className="col-1"></div>
           </div>
         </div>
         <div
           className="text-center p-3"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
         >
-          © 2023 Copyright: Instituto Trabalho e Vida
+          © 2023 - Instituto Para Promoção do Trabalho Empreendedor - Trabalho e
+          Vida
         </div>
       </footer>
     );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import SimposioImg from "../assets/images/simposio.jpeg";
 
 class About extends Component {
   render() {
     return (
       <div>
         <h4 className="page-title p-3">Sobre nós</h4>
+        <div className="row p-3">
+          <div className="col-5">
+            <img src={SimposioImg} alt="Simposio" style={{ maxWidth: 350 }} />
+          </div>
+          <div className="col-6">
+            <p>
+              O Instituto Para Promoção do Trabalho Empreendedor –{" "}
+              <strong>Trabalho e Vida</strong> foi fundado em 1º de março de
+              2008 com o objetivo de operacionalizar e organizar eventos,
+              palestras, seminários e congressos possibilitando ao seu público
+              ter acesso aos mais diversificados temas e informações sobre
+              capacitação, empreendedorismo e principalmente qualidade de vida
+              no mundo do trabalho.
+              <br />
+              <br />
+              <strong>Nossa visão</strong> é de que a sociedade pode se
+              organizar melhor quando adota a disseminação de cultura do
+              trabalho nas diferentes fases da vida gerando produtividade e bem
+              estar social individual e coletivo.
+              <br />
+              <br />
+              <strong>Nossa missão</strong> é atuar como facilitador das ações
+              de empreendedorismo, seja na divulgação de novas tecnologias
+              simplificadoras do trabalho, ou ainda, na formulação e execução de
+              projetos de qualidade de vida relacionados ao trabalho nas
+              diversas atividades laborais, propondo mudanças no planejamento
+              das ações das empresas, empregadores e da sociedade como um todo.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
