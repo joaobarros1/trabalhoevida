a768f3c2299a1e20712bc23487c9b855ab0f5064
joaocorreia1 <jpedrocbarros@gmail.com>
2023-07-16 19:39:02 -0300

    evento

--- src/pages/Partners.js
+++ src/pages/Partners.js
@@ -4,13 +4,16 @@ import unnamed1 from "../assets/images/unnamed (1).jpg";
 import unnamed2 from "../assets/images/unnamed (2).jpg";
 import unnamed3 from "../assets/images/unnamed (3).jpg";
 import unnamed4 from "../assets/images/unnamed (4).jpg";
+import abece1 from "../assets/images/abece1.jpg";
+import abece2 from "../assets/images/abece2.jpg";
+import abece from "../assets/images/abece.jpg";
 
 export class NextEvents extends Component {
   render() {
     return (
       <div className="d-flex page-container">
+        <h4 className="page-title p-3 my-3">Eventos de Parceiros</h4>
         <div className="d-flex flex-column">
-          <h4 className="page-title p-3 mt-5">Eventos de Parceiros</h4>
           <div className="container">
             <div className="d-flex flex-column align-items-center">
               <img src={unnamed} alt="banner evento" />
@@ -20,6 +23,19 @@ export class NextEvents extends Component {
               <img src={unnamed4} alt="banner evento" />
               <a
                 href="https://inscricaodeeventos.com.br/informa/concreteshow/2023/usuario/index.asp?vendedor=ABCIC"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className="btn btn-primary m-3">Inscreva-se</button>
+              </a>
+            </div>
+            <hr class="hr" />
+            <div className="d-flex flex-column align-items-center">
+              <img src={abece1} alt="banner evento" />
+              <img src={abece2} alt="banner evento" />
+              <img src={abece} alt="banner evento" />
+              <a
+                href="https://site.abece.com.br/formulario-6o-simposio-abece-universidades-de-engenharia-de-estruturas-aplicacoes-do-concreto-de-ultra-alto-desempenho/"
                 target="_blank"
                 rel="noreferrer"
               >
